3d9a973baf332de7852cc4af7f746ea38c570677
Abdulaki <147936105+abex-COM@users.noreply.github.com>
2024-03-03 22:06:58 +0300

    add js to it

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("input");
+const clear = document.querySelector(".clear");
+const deleteAll = document.querySelector(".allClear");
+const result = document.querySelector(".results");
+const display = document.querySelector(".display");
+
+
+result.addEventListener(("click"), () => {
+    display.textContent = eval(input.value);
+
+})
+clear.addEventListener(("click"), () => {
+    const currentValue = input.value;
+    input.value = currentValue.slice(0, -1);
+})
+deleteAll.addEventListener(("click"), () => {
+    input.value = '';
+    display.textContent='0'
+})
+ function calculate(value) {
+     input.value += value;
+ }
+
